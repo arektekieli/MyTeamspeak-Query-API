5401266bf5ad148fd12d2db78563c7aa2a8115a4
arektekieli <arektekieli@gmail.com>
2019-12-24 14:41:18 +0100

    clientinfo

--- TeamspeakAPI.py
+++ TeamspeakAPI.py
@@ -5,9 +5,31 @@ class TeamspeakAPI(Teamspeak):
     def __init__(self, host, port, sid, login, password, nick='ArasBot'):
         super().__init__(host, port, sid, login, password, nick)
 
-    def getClientList(self):
-        result = self.query('clientlist')
+    def safeQuery(self, query):
+        result = self.query(query)
+
+        if self.lastErrorId != 0:
+            return False
+
+        return result 
+
+    def clientinfo(self, client):
+        # Returns TeamspeakResultItem object
+        # client must be clid or TeamspeakClient class
+        try:
+            if isinstance(client, TeamspeakClient):
+                return self.safeQuery('clientinfo clid={}'.format(client.clid)).items[0]
+            else:
+                return self.safeQuery('clientinfo clid={}'.format(client)).items[0]
+        except:
+            return False
+
+    def clientlist(self):
+        # Returns list of TeamspeakClient objects
+        result = self.safeQuery('clientlist')
         if result == False:
             return False
         
         return [TeamspeakClient(self, x) for x in result.fetch()]
+        
+        
--- TeamspeakResultItem.py
+++ TeamspeakResultItem.py
@@ -3,10 +3,7 @@ from TeamspeakAbstract import TeamspeakAbstract
 class TeamspeakResultItem(TeamspeakAbstract):
     def __init__(self, args):
         super().__init__()
-        tmp = []
         for arg in args:
             for key, val in arg.items():
-                tmp.append(key)
                 setattr(self, key, val)
-        print(tmp)
         
